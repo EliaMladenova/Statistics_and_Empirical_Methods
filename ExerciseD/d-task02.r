0e8039c16adf4b2eb53d5430c2c5ec2e54f12d39
Елиа <50628441+EliaMladenova@users.noreply.github.com>
2020-11-27 14:36:33 +0200

    Create d-task02.r

---
+++ ExerciseD/d-task02.r
@@ -0,0 +1,6 @@
+> dnbinom(5, size = 3, prob = 0.2)
+> pnbinom(5, size = 3, prob = 0.2) - pnbinom(4, size = 3, prob = 0.2)
+
+> pnbinom(3 , size = 3, prob = 0.2, lower.tail = FALSE)
+
+> pnbinom(5 , size = 3, prob = 0.2) - pnbinom(1 , size = 3, prob = 0.2)
